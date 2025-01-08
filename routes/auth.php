14be93e2b9bf16096618914b188872483c5a947e
hapo-daidt <daidt@haposoft.com>
2025-01-08 23:58:20 +0700

    Config name + email

--- routes/auth.php
+++ routes/auth.php
@@ -9,5 +9,5 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
